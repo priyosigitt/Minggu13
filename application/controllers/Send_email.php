fb56904a7797cd805f420d46edb5f1d76775ee07
priyosigitt <xiaogit@gmail.com>
2020-06-18 16:54:22 +0700

    Minggu13

---
+++ application/controllers/Send_email.php
@@ -0,0 +1,53 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Send_email extends CI_Controller {
+
+    /**
+     * Kirim email dengan SMTP Gmail.
+     *
+     */
+    public function index()
+    {
+      // Konfigurasi email
+        $config = [
+            'mailtype'  => 'html',
+            'charset'   => 'utf-8',
+            'protocol'  => 'smtp',
+            'smtp_host' => 'smtp.gmail.com',
+            'smtp_user' => '122201802711@mhs.dinus.ac.id',  // Email gmail
+            'smtp_pass'   => 'masuk123',  // Password gmail
+            'smtp_crypto' => 'ssl',
+            'smtp_port'   => 465,
+            'crlf'    => "\r\n",
+            'newline' => "\r\n"
+        ];
+
+        // Load library email dan konfigurasinya
+        $this->load->library('email', $config);
+
+        // Email dan nama pengirim
+        $this->email->from('no-reply@udinus.com', 'udinus.com');
+
+        // Email penerima
+        $this->email->to('122201802711@mhs.dinus.ac.id'); // Ganti dengan email tujuan
+
+        // Lampiran email, isi dengan url/path file
+        $this->email->attach('File.png');
+
+        // Subject email
+        $this->email->subject('Kirim Email dengan SMTP Gmail CodeIgniter | udinus.com');
+
+        // Isi email
+        $this->email->message("Ini adalah contoh email CodeIgniter yang dikirim menggunakan SMTP email Google (Gmail).<br><br> Klik <strong><a>
+href='https://dinsutek.com/masaboe/materi' target='_blank'
+rel='noopener'>disini</a></strong> untuk melihat tutorialnya.");
+
+        // Tampilkan pesan sukses atau error
+        if ($this->email->send()) {
+            echo 'Sukses! email berhasil dikirim.';
+        } else {
+            echo 'Error! email tidak dapat dikirim.';
+        }
+    }
+}
